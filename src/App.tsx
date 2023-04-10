581aaf6bb193086b8b7a69334ff0d42044d88a6a
HenriqueContini <henrique.coutine@gmail.com>
2023-04-10 14:06:19 -0300

    Feat: Header

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import style from './App.module.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className={style.app}>
+      <Header></Header>
+    </div>
+  )
+}
+
+export default App
