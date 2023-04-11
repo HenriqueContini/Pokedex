0dc85921a01e1d1b08cfd30cfebf433977330058
HenriqueContini <henrique.coutine@gmail.com>
2023-04-11 14:27:16 -0300

    Feat: PokemonList

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import style from './App.module.css';
 import Header from './components/Header';
+import PokemonList from './components/PokemonList';
 
 function App() {
   return (
     <div className={style.app}>
-      <Header></Header>
+      <Header />
+      <PokemonList />
     </div>
   )
 }
---
+++ src/components/PokemonList.tsx
@@ -0,0 +1,11 @@
+import style from './PokemonList.module.css'
+
+const PokemonList = () => {
+  return (
+    <section className={style.pokemonList}>
+      
+    </section>
+  )
+}
+
+export default PokemonList
